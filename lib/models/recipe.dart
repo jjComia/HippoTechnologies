37db08210e5faab5c706bb1371f2ed994f61190e
ntomlin7 <n.tomlin@outlook.com>
2024-10-02 15:17:13 -0500

    commit to sync up with Ingredients

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -7,6 +7,7 @@ class Recipe {
   final String prepUnit;
   final int cookTime;
   final String cookUnit;
+  //generic comment
 
   Recipe({required this.id, required this.name, required this.description, required this.rating, required this.prepTime, required this.prepUnit, required this.cookTime, required this.cookUnit});
 
